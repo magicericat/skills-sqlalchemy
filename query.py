0925761b17540fc5deeca280b3b964488a8afcfe
magicericat <ericatj@gmail.com>
2015-10-25 20:13:11 -0700

    Part 2 and Part 3 complete.

--- query.py
+++ query.py
@@ -22,20 +22,34 @@ init_app()
 # Part 2: Write queries
 
 # Get the brand with the **id** of 8.
+Brand.query.filter_by(id=8).first()
+  
 
 # Get all models with the **name** Corvette and the **brand_name** Chevrolet.
+Model.query.filter_by(name="Corvette", brand_name="Chevrolet").all()
 
 # Get all models that are older than 1960.
+Model.query.filter(Model.year < 1960).all()
+
 
 # Get all brands that were founded after 1920.
+Brand.query.filter(Brand.founded > 1920).all()
+
 
 # Get all models with names that begin with "Cor".
+Model.query.filter(Model.name.like("Cor%")).all()
+
 
 # Get all brands with that were founded in 1903 and that are not yet discontinued.
+Brand.query.filter(Brand.founded == 1903, Brand.discontinued == None).all()
+
 
 # Get all brands with that are either discontinued or founded before 1950.
+Brand.query.filter(db.or_(Brand.discontinued != None, Brand.founded < 1950)).all()
 
 # Get any model whose brand_name is not Chevrolet.
+Model.query.filter(Model.brand_name != 'Chevrolet').all()
+
 
 # Fill in the following functions. (See directions for more info.)
 
@@ -43,20 +57,26 @@ def get_model_info(year):
     '''Takes in a year, and prints out each model, brand_name, and brand
     headquarters for that year using only ONE database query.'''
 
-    pass
+    models = db.session.query(Model.name, Model.brand_name, Brand.headquarters).join(Brand, Model.brand_name==Brand.name).filter(Model.year == year).all()
+    
+    for name, brand, headquarters in models:
+        print "Model: %s  Brand: %s  Headquarters: %s" % (name, brand, headquarters)
 
 def get_brands_summary():
     '''Prints out each brand name, and each model name for that brand
      using only ONE database query.'''
 
-    pass
+    brands = db.session.query(Brand.name, Model.name).outerjoin(Model,Model.brand_name==Brand.name).order_by(Brand.name).all()
+    
+    for brand, model in brands:
+        print "Brand name: %s  Model: %r" % (brand, model)
 
 # -------------------------------------------------------------------
 
 
 # Part 2.5: Advanced and Optional
 def search_brands_by_name(mystr):
-    pass
+    
 
 
 def get_models_between(start_year, end_year):
@@ -68,5 +88,15 @@ def get_models_between(start_year, end_year):
 
 # 1. What is the returned value and datatype of ``Brand.query.filter_by(name='Ford')``?
 
+# This is a query that returns a flask_sqlalchemy.BaseQuery object. It is a
+# "question" without the "answer." To get a Brand object returned by the query,
+# you'd need to add .one() to fetch the record.
+
+
 # 2. In your own words, what is an association table, and what *type* of relationship
 # does an association table manage?
+
+# An association table only exists to link things together. An association table
+# manages tables that have many-to-many relationships & it stores the  primary 
+# keys of the tables it connects as foreign keys.
+
